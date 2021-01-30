30a07636dc460f8dc419a04adc05f5b0b0aee690
muse-gabs <45804707+muse-gabs@users.noreply.github.com>
2021-01-29 22:10:18 -0700

    added sample.py
    to check that opencv can be imported correctly.

---
+++ sample.py
@@ -0,0 +1,9 @@
+# test whether opencv was successfully installed
+# import doesn't through any errors
+import cv2
+
+# just a test to see what version of opencv is installed
+# prints the version of cv to the console when run
+print(cv2.__version__)
+
+
